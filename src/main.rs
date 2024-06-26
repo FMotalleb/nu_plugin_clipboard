91dfc31567346da1c7dc65ffbc63ac163bb84400
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-06-27 00:07:18 +0330

    minor: bump nu deps to v0.95

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 mod clipboard;
 
 pub mod utils;
-use nu_plugin::{PluginCommand};
 use crate::clipboard::copy::ClipboardCopy;
 use crate::clipboard::paste::ClipboardPaste;
+use nu_plugin::PluginCommand;
 
 pub struct ClipboardPlugins;
 
 impl nu_plugin::Plugin for ClipboardPlugins {
-    fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin=Self>>> {
+    fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![
             Box::new(ClipboardCopy::new()),
             Box::new(ClipboardPaste::new()),
         ]
     }
-}
 
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
+}
 
 fn main() {
     if ClipboardCopy::is_daemon() {

105f9383fe9f04792c3fc76e7a186d77fc1524f2
FMotalleb <m.falahnejad@dornica.net>
2025-05-05 15:50:54 +0330

    fix: daemon mode failed on huge input, switched to stdin instead of arg to address this issue

--- src/clipboard/linux.rs
+++ src/clipboard/linux.rs
@@ -1,6 +1,6 @@
 use std::{
     env,
-    io::{stderr, stdout},
+    io::{stderr, stdout, Read, Write},
     process::{Command, Stdio},
 };
 
@@ -9,7 +9,7 @@ use super::{
     clipboard::{CheckResult, Clipboard},
 };
 
-use arboard::SetExtLinux;
+use arboard::Error;
 use nu_protocol::LabeledError;
 
 const DAEMONIZE_ARG: &str = "daemon-copy";
@@ -26,31 +26,57 @@ impl ClipBoardLinux {
         env::args().nth(1).as_deref() == Some(DAEMONIZE_ARG)
     }
     fn request_daemon(&self, text: &str) -> Result<(), nu_protocol::LabeledError> {
-        match env::current_exe().map(|exe| {
-            Command::new(exe)
-                .arg(DAEMONIZE_ARG)
-                .arg(text)
-                .stdin(Stdio::null())
-                .stdout(stdout())
-                .stderr(stderr())
-                .current_dir(env::temp_dir())
-                .spawn()
-        }) {
-            Ok(Ok(_)) => Ok(()),
-            Err(err) | Ok(Err(err)) => Err(nu_protocol::LabeledError::new(format!(
+        match env::current_exe().map(|exe| spawn_daemon(text, exe)) {
+            Ok(res) => res,
+            Err(err) => Err(nu_protocol::LabeledError::new(format!(
                 "Failed to spawn daemon process: {}",
                 err.to_string()
             ))),
         }
     }
-    fn copy_with_daemon() -> Result<(), nu_protocol::LabeledError> {
+    fn copy_with_daemon() -> Result<(), LabeledError> {
         with_clipboard_instance(|clip: &mut arboard::Clipboard| {
             clip.clear()?;
-            let args: Vec<String> = env::args().skip(2).collect();
-            let data = args.join(" ");
-            clip.set().wait().text(data)
+            let mut input = String::new();
+            if let Err(err) = std::io::stdin().read_to_string(&mut input) {
+                return Err(Error::Unknown {
+                    description: format!("Failed to read from stdin: {}", err.to_string()),
+                });
+            }
+            clip.set_text(input)
         })
     }
+}
+
+fn spawn_daemon(text: &str, exe: std::path::PathBuf) -> Result<(), LabeledError> {
+    let child = Command::new(exe)
+        .arg(DAEMONIZE_ARG)
+        .stdin(Stdio::piped())
+        .stdout(stdout())
+        .stderr(stderr())
+        .current_dir(env::temp_dir())
+        .spawn();
+    if let Err(err) = child {
+        return Err(LabeledError::new(format!(
+            "failed to spawn daemon process: {}",
+            err
+        )));
+    }
+    let mut child = child.unwrap();
+    if let Some(mut stdin) = child.stdin.take() {
+        if let Err(err) = stdin.write_all(text.as_bytes()) {
+            return Err(LabeledError::new(format!(
+                "Failed to write to stdin: {}",
+                err
+            )));
+        }
+    }
+
+    // Optionally, wait for the child process to finish
+    let status = child.wait();
+    status
+        .map(|_| ())
+        .map_err(|e| LabeledError::new(e.to_string()))
 }
 
 impl Clipboard for ClipBoardLinux {
